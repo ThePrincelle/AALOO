5c23f31fb6516573aaa2e9043edab9f697f382d5
Maxime Princelle <maxime@princelle.org>
2022-02-02 14:36:47 +0100

    👷 adding export formats

--- generate.sh
+++ generate.sh
@@ -1,16 +1,30 @@
 # generate.sh
-# Script that generates the diagrams (*.mdd) output
+# Script that generates the diagrams (*.mmd) output
 # --------------------------------
 
-# List all files with *.mdd extension
-for file in *.mdd
+# List all files with *.mmd extension
+for file in *.mmd
 do
+    # Echo message
+    echo "Working on $file..."
+
     # Extract filename without extension
-    filename=$(basename "$file" .mdd)
+    filename=$(basename "$file" .mmd)
 
     # Output folder
-    mkdir output
+    mkdir -p output/$filename
 
     # Generate the diagram with mmdc
-    ./node_modules/.bin/mmdc -i $file -o output/$filename.png -t dark -b transparent
+
+    # PNG
+    echo "> PNG..."
+    ./node_modules/.bin/mmdc -i $file -o output/$filename/$filename.png -t dark -w 8000 -H 6000 -b transparent
+
+    # PDF
+    echo "> PDF..."
+    ./node_modules/.bin/mmdc -i $file -o output/$filename/$filename.pdf
+
+    # SVG
+    echo "> SVG..."
+    ./node_modules/.bin/mmdc -i $file -o output/$filename/$filename.svg -t dark
 done
