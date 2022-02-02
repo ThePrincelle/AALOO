da17c0ffd6e3804c5cf32d51e684eb6cd8ffb607
Maxime Princelle <maxime@princelle.org>
2022-02-02 16:14:49 +0100

    ✨ update stuff

--- README.md
+++ README.md
@@ -8,21 +8,24 @@ Analyse et architecture logicielle orientée objets.
 - Nicolas LAFORÊT
 - Maxime PRINCELLE
 
-
 Les diagrammes de ce README se mettent à jour automatiquement, vous pouvez voir si une mise à jour est en cours en fonction de l'état de ce badge :
 
 [![pipeline status](https://git.unistra.fr/princelle/aaloo/badges/main/pipeline.svg)](https://git.unistra.fr/princelle/aaloo/-/commits/main)
 
-## Diagrammes
+Tous les diagrammes sont disponibles au téléchargement à [cette adresse](https://git.unistra.fr/princelle/aaloo/-/jobs/artifacts/main/download?job=build).
 
-Vous trouverez ici les diagrammes pour le module AALOO.
+## TP 1
 
-Ces derniers sont disponibles au téléchargement à [cette adresse](https://git.unistra.fr/princelle/aaloo/-/jobs/artifacts/main/download?job=build).
+L'objectif de ce TP est de développer une analyse et d'architecture logicielle orientée objets sur le projet [LuxCore](https://github.com/LuxCoreRender/LuxCore).
+
+Lien vers le sujet : [LuxCoreRender - TP1.pdf](https://git.unistra.fr/princelle/aaloo/-/blob/main/TP1/LuxCoreRender - TP1.pdf)
+
+### Diagrammes
 
 ### main
 
-![Diagramme Main](https://git.unistra.fr/princelle/aaloo/-/jobs/artifacts/main/raw/output/main/main.png?job=build)
+![Diagramme Main](https://git.unistra.fr/princelle/aaloo/-/jobs/artifacts/main/raw/output/TP1/main/main.png?job=build)
 
 ### startLockLess
 
-![Diagramme startLockLess](https://git.unistra.fr/princelle/aaloo/-/jobs/artifacts/main/raw/output/startLockLess/startLockLess.png?job=build)
+![Diagramme startLockLess](https://git.unistra.fr/princelle/aaloo/-/jobs/artifacts/main/raw/output/TP1/startLockLess/startLockLess.png?job=build)
--- generate.sh
+++ generate.sh
@@ -2,29 +2,38 @@
 # Script that generates the diagrams (*.mmd) output
 # --------------------------------
 
-# List all files with *.mmd extension
-for file in *.mmd
-do
-    # Echo message
-    echo "Working on $file..."
+# For each TP* folder, generate the diagrams
+for i in TP*; do
+    echo "Generating diagrams for $i..."
+    cd $i
 
-    # Extract filename without extension
-    filename=$(basename "$file" .mmd)
+    # List all files with *.mmd extension
+    for file in *.mmd
+    do
+        # Echo message
+        echo "> Working on $file..."
 
-    # Output folder
-    mkdir -p output/$filename
+        # Extract filename without extension
+        filename=$(basename "$file" .mmd)
 
-    # Generate the diagram with mmdc
+        # Output folder
+        mkdir -p ../output/$i/$filename
 
-    # PNG
-    echo "> PNG..."
-    docker run -u root -v $(pwd):/data minlag/mermaid-cli -i /data/$file -o /data/output/$filename/$filename.png -t dark -w 8000 -H 6000 -b transparent
+        # Go back to previous directory
+        cd ..
 
-    # PDF
-    echo "> PDF..."
-    docker run -u root -v $(pwd):/data minlag/mermaid-cli -i /data/$file -o /data/output/$filename/$filename.pdf
+        # Generate the diagram with mmd
 
-    # SVG
-    echo "> SVG..."
-    docker run -u root -v $(pwd):/data minlag/mermaid-cli -i /data/$file -o /data/output/$filename/$filename.svg -t dark
+        # PNG
+        echo "--> PNG..."
+        docker run -u root -v $(pwd):/data minlag/mermaid-cli -i /data/$i/$file -o /data/output/$i/$filename/$filename.png -t dark -w 8000 -H 6000 -b transparent
+
+        # PDF
+        echo "--> PDF..."
+        docker run -u root -v $(pwd):/data minlag/mermaid-cli -i /data/$i/$file -o /data/output/$i/$filename/$filename.pdf
+
+        # SVG
+        echo "--> SVG..."
+        docker run -u root -v $(pwd):/data minlag/mermaid-cli -i /data/$i/$file -o /data/output/$i/$filename/$filename.svg -t dark
+    done
 done
